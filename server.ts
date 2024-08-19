55d7f72fdeb29d08051f568bf3cb42d8bacddaf6
nicohrubec <nicolas.hrubec@outlook.com>
2024-08-19 11:24:30 +0200

    Add loop throwing error

--- server.ts
+++ server.ts
@@ -4,6 +4,14 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = createServer((req, res) => {
+  for (let i = 0; i < 3; i ++) {
+    console.log('i ==', i);
+    if (i == 2) {
+      throw new Error();
+    }
+  }
+
+
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
